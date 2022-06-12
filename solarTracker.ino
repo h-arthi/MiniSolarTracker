f195a9b60f0ff0bf99d026388e83bd0192d1fbbf
h-arthi <harthi.ganesh@live.com>
2022-06-12 21:10:06 +0800

    added PID but not yet tested with real setup

--- solarTracker.ino
+++ solarTracker.ino
@@ -5,10 +5,21 @@ int TR = A0;//top right ldr
 int BR = A3;//bottom right ldr
 int TL = A1;//top left ldr
 int BL = A2;//bottom left
+
 //initialize the servo
 Servo myservo;
 int servoPin = 11;//the analog servo pin
-int initialStage = 70;//initial value for the servo 
+int initialStage = 90;//initial value for the servo
+int avg;//average largest sun
+int servoVal;
+int error;
+int derivative;
+//PID Variables
+int lastError = 0; //setting the last error to 0
+int Kd = 5;//setting a derivative constant
+int Kp = 20;//setting a proportional contstant
+
+
 void setup() {
   myservo.attach(servoPin);//setting the servo to this pin number
   pinMode(TR, INPUT);
@@ -16,6 +27,7 @@ void setup() {
   pinMode(TL, INPUT);
   pinMode(BL, INPUT);
   myservo.write(initialStage);//setting the servo to move 90 degree
+  Serial.begin(9600);
   delay (1000);
 }
 
@@ -26,10 +38,44 @@ void loop() {
   int top2 = analogRead(BR);
   int top3 = analogRead(TL);
   int top4 = analogRead(BL);
-  int avgR = (top1 + top3);
-  int avgL = (top2 + top4);
+  int avgR = (top1 + top2)/2;
+  int avgL = (top3 + top4)/2;
+  
+  Serial.println(avgR);
+  Serial.println(avgL);
+  
+  
+  //if-else statements to change th e servo position
+  if (avgR > avgL)
+  {
+  	avg = avgR;
+    //proportional term
+  	error = 997 - avg;
+  	//derivative term
+  	derivative = error - lastError;
+    //setting the servo to move accordingly
+  	servoVal = initialStage - (error * Kp + derivative * Kd); 
+    myservo.write(servoVal);  
+  }
+  
+  else if (avgL > avgR)
+  {
+  	avg = avgL;
+    //proportional term
+  	error = 997 - avg;
+  	//derivative term
+  	derivative = error - lastError;
+    //setting the servo to move accordingly
+  	servoVal = initialStage + (error * Kp + derivative * Kd); 
+    myservo.write(servoVal); 
+  }
+  
+  
+  lastError = error;
+  
+  
 
-  //using relational operators to compare the values
+  /*//using relational operators to compare the values
    if (avgR > avgL)
    {
     myservo.write(initialStage-20);
@@ -39,7 +85,7 @@ void loop() {
    {
     myservo.write(initialStage+20);
     delay(100);
-   }
+   }*/
 
 
 
